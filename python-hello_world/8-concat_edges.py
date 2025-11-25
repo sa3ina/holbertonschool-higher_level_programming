9477d3181aa70c187600b86003647d72338295b4
sa3ina <sravnennaya@mail.ru>
2025-11-25 11:58:56 +0000

    h

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-word=str.split()
-print(word[5]+ " " + word[6]+ " " +word[12] + " " +word[0])
+print(str.split()[5], str.split()[6], str.split()[12], str.split()[0])
+
