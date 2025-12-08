8b5c1c5615804ed9b8757687c204e5858f52d94c
sa3ina <sravnennaya@mail.ru>
2025-12-08 12:53:39 +0000

    k

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    for x in a_dictionary:
+        if x == key:
+            a_dictionary[x] = value
+        else:
+            continue
+    return a_dictionary
