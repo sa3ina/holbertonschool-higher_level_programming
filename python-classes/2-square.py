c4b647db9b86ca8f1c07123d181b457b0b91bd48
sa3ina <sravennaya@mail.ru>
2025-12-18 11:09:57 +0000

    r

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This defines geometric shapes"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This function checks type validation"""
+        try:
+            self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
