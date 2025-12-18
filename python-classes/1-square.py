50b3fd9b17ecd7fb234e75bc3da07a3cafdec7a9
sa3ina <sravennaya@mail.ru>
2025-12-18 10:56:52 +0000

    o

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module represents geometric shapes"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size):
+        """This defines a square size"""
+        self.__size = size
