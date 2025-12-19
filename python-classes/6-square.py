c2e8124b101649e2cf9a4b1b7ce87499a7f5be66
sa3ina <sravnennaya@mail.ru>
2025-12-19 14:12:35 +0000

    y

---
+++ python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This defines geometric shapes"""
+
+
+class Square:
+    """This class shows coordinates of a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (
+            not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(x, int) and x >= 0 for x in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self._position[1]):
+            print("")
+        for _ in range(self.__size):
+            print(" "*self.__position[0] + "#"*self.__size)
