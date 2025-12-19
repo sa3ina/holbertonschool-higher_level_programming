5a3e9d13af267e0d1f7bf5f2e3e398d02246a2ad
sa3ina <sravnennaya@mail.ru>
2025-12-19 12:06:27 +0000

    r

---
+++ python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This defines geometric shapes"""
+
+
+class Square:
+    """This class prints a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if size == 0:
+            print("")
+        for i in range(self.__size):
+            print("#"*self.__size)
