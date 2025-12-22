48896018841f90789807a2222dcfa093dacec3cb
sa3ina <sravnennaya@mail.ru>
2025-12-22 19:09:34 +0000

    p

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""This module represents inheritance"""
+
+
 def lookup(obj):
+    """This function returns the list of avaliable attributes
+    and methods of an object"""
+
     return dir(obj)
