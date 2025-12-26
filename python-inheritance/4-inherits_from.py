d566a766d8c1ade15d980476565d60cb0f298d98
sa3ina <sravnennaya@mail.ru>
2025-12-26 18:02:17 +0000

    e

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """This module represents inheritance"""
 
 
-def is_same_class(obj, a_class):
+def inherits_from(obj, a_class):
     """This class returns true if object is instance of specified class"""
 
     return issubclass(obj, a_class)
