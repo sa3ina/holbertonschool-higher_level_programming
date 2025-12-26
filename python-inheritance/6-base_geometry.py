466803de69d472dc3e37af776275ce528a985d3e
sa3ina <sravnennaya@mail.ru>
2025-12-26 18:18:25 +0000

    q

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module represents Geometry module"""
+
+
+class BaseGeometry:
+    """This class represents BaseGeometry"""
+
+    def area(self):
+        """This function calculates area"""
+        raise Exception("area() is not implemented")
