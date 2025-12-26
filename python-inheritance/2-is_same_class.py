2967eae143a03bf13658e5cf3fe61eb971ad6495
sa3ina <sravnennaya@mail.ru>
2025-12-26 14:27:37 +0000

    h

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module represents inheritance"""
+
+
+def is_same_class(obj, a_class):
+    """This class returns true if object is specified class"""
+
+    return type(obj) is a_class
