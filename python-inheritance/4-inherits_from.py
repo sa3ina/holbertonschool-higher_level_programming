db545c9ee4cc97aa5df8572df24172911c21576b
sa3ina <sravnennaya@mail.ru>
2025-12-26 18:01:04 +0000

    e

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module represents inheritance"""
+
+
+def is_same_class(obj, a_class):
+    """This class returns true if object is instance of specified class"""
+
+    return issubclass(obj, a_class)
