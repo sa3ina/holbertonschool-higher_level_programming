10cd00eb12a9e30c969cc000f0c4b8f3a05e4171
sa3ina <sravnennaya@mail.ru>
2025-12-26 17:58:14 +0000

    u

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module represents inheritance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This class returns true if object is instance of a class"""
+
+    return isinstance(obj, a_class)
