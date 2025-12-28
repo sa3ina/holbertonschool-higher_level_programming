ccd2860a93b9c7fcd24081a6bf08a5480c9c8ece
sa3ina <sravnennaya@mail.ru>
2025-12-28 17:59:33 +0000

    e

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(count))
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        value = self[index]
+        print("Popped [{}] from the list.".format(value))
+        return super().pop(value)
