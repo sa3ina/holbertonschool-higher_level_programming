2c279aa10fa4167ffc92598438124e048a16ff2e
sa3ina <sravnennaya@mail.ru>
2026-01-04 16:29:47 +0000

    a

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module represents operations with files"""
+
+
+class Student:
+    """This is a Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            dict = {}
+            for i in attrs:
+                if isinstance(i, str) and hasattr(self, i):
+                    dict[i] = getattr(self, i)
+            return dict
+        return self.__dict__
