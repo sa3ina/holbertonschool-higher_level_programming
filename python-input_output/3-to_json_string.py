0964805c54d0ada64aaa01984146a9219a48303e
sa3ina <sravnennaya@mail.ru>
2026-01-04 15:21:44 +0000

    h

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""This module represents operations with files"""
+
+
+def to_json_string(my_obj):
+    """This function returns the JSON repr"""
+    return json.dumps(my_obj)
