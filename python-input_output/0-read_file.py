583fed6b67c021fbac9966291187cb5a3ddb3cb8
sa3ina <sravnennaya@mail.ru>
2026-01-04 14:47:25 +0000

    r

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""This module represents operations with files"""
+
 
 def read_file(filename=""):
+    """This function reads a file"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
