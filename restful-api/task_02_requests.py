1071b455949913a1f7443e35b0fa2c5765d1592a
sa3ina <sravnennaya@mail.ru>
2026-01-08 12:47:44 +0000

    t

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,38 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post["title"])
+
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        structured_posts = []
+
+        for post in posts:
+            structured_posts.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+                })
+
+        with open("posts.csv", "w", newline="") as csvfile:
+            writer = csv.DictWriter(
+                    csvfile,
+                    fieldnames=["id", "title", "body"],
+            )
+            writer.writeheader()
+            writer.writerows(structured_posts)
