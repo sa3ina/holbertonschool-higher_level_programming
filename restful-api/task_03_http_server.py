afd03c770d487f142393929f59b61f88269c4f99
sa3ina <sravnennaya@mail.ru>
2026-01-10 19:30:19 +0000

    r

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+    def run():
+        server_adress = ("", 8000)
+        httpd = HTTPServer(server_adress, SimpleAPIHandler)
+        httpd.serve_forever()
+
+    if __name__ == "__main__":
+        run()
