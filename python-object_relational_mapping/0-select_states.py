5a6b38ffb36fd1eeee199e24115fe84cde227a9f
sa3ina <sravnennaya@mail.ru>
2026-01-25 16:50:59 +0000

    e

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-Lists all states from a database hbtn_0e_0_usa
-"""
 import MySQLdb
 import sys
 
@@ -10,22 +7,18 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
 
-    # Connect to MySQL
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=user,
-                           passwd=password,
-                           db=db,
-                           charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db,
+        charset="utf8"
+    )
+
     cur = conn.cursor()
-
-    # Execute query - select all states and order by id ascending
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all rows and print
     for row in cur.fetchall():
         print(row)
-
-    # Close cursor and connection
     cur.close()
     conn.close()
