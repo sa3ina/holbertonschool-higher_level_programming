2d37ee8e1718953b39a5fea2f5171c517e9046be
sa3ina <sravnennaya@mail.ru>
2026-01-25 16:48:11 +0000

    e

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,31 @@
 #!/usr/bin/python3
 """
-    Script that lists all states from the database.
+Lists all states from a database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
-
-
-def connectDb(user, password, db):
-    """
-        Get connection with the database.
-        Args:
-            user (str): Username of the user.
-            password (str): Password of the user.
-            db (str): Database to retrieve.
-        Return:
-            Connection database.
-    """
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=user,
-        passwd=password,
-        db=db,
-        charset="utf8"
-    )
-    return conn
-
 
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     db = sys.argv[3]
 
-    conn = connectDb(user, password, db)
+    # Connect to MySQL
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=user,
+                           passwd=password,
+                           db=db,
+                           charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+
+    # Execute query - select all states and order by id ascending
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows and print
+    for row in cur.fetchall():
         print(row)
+
+    # Close cursor and connection
     cur.close()
     conn.close()
