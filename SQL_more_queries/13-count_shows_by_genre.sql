727c8f99d554584fb4b2e4fc44a13ec44ad2ccd1
sa3ina <sravnennaya@mail.ru>
2026-01-25 15:38:21 +0000

    s

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Genre and number of shows
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
