7953606c4d0c9ddaaafd324de8744fe6bc307b38
sa3ina <sravnennaya@mail.ru>
2026-02-01 23:15:13 +0400

    task added

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg1 = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(arg1));
