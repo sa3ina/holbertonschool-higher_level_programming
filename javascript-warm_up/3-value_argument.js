b259220517db7e6993328c0b93db24e91ffcffd5
sa3ina <sravnennaya@mail.ru>
2026-02-01 22:44:24 +0400

    task added

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 
-const argsCount = process.argv.length;
 const firstArg = process.argv[2];
 
-if (argsCount === 2) {
+if (argsCount === undefined) {
   console.log('No argument');
 } else {
   console.log(firstArg);
