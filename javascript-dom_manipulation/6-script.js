6c1906713b3865ce1a0e2da6e61da11d8dd965e4
sa3ina <sravnennaya@mail.ru>
2026-02-06 18:53:56 +0400

    task added

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+const character = document.getElementById('character');
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        character.textContent = data.name;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
