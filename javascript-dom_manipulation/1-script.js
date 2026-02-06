6f12011a07446a256d8335ab1b85f868cca3eb55
sa3ina <sravnennaya@mail.ru>
2026-02-06 18:29:36 +0400

    task added

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+const red_header = document.getElementById('red_header');
+const header = document.querySelector('header');
+red_header.addEventListener('click', function () {
+    header.style.color = 'red';
+});
