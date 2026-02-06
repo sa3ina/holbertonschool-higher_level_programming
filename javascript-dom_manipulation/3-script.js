8731e99a2adadba719a7154e39522124513d0699
sa3ina <sravnennaya@mail.ru>
2026-02-06 18:39:10 +0400

    task added

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header');
+const toggle_header = document.getElementById('toggle_header');
+toggle_header.addEventListener('click', function () {
+    header.classList.toggle('red');
+    header.classList.toggle('green');
+});
