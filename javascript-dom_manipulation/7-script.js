84c0a381db0c273d0332750405e24a40a14ee6fe
sa3ina <sravnennaya@mail.ru>
2026-02-06 19:01:50 +0400

    task added

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+const listMovies = document.getElementById('list_movies');
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(movie => {
+            const li = document.createElement('li');
+            li.textContent = movie.title;
+            listMovies.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
