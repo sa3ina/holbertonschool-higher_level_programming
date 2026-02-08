fa301a0e082bbafa6501f195aaa17ccefdfb30c5
sa3ina <sravnennaya@mail.ru>
2026-02-08 23:21:17 +0400

    task

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,19 +1,28 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/items')
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/items")
 def items():
-    with open('items.json') as f:
-        data = json.load(f)
-        items_list = data.get("items", [])
+    try:
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+            items_list = data.get("items", [])
+    except FileNotFoundError:
+        items_list=[]
+    return render_template("items.html", items=items_list)
 
-    return render_template('items.html', items=items_list)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
